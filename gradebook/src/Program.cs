e2901d04b2d0d8147f472e0f00e82d4d27b46bde
Lopes Boa <lopesboa1@gmail.com>
2020-11-12 22:36:03 -0300

    adding numbers & creating arrays

---
+++ gradebook/src/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace src
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var numbers = new double[3];
+      numbers[0] = 12.9;
+      if (args.Length > 0)
+      {
+        Console.WriteLine($"Hello {args[0]}!");
+      }
+      else
+      {
+        Console.WriteLine("Hello World!");
+      }
+    }
+  }
+}
