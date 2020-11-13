b40305cd7355c89c87816eebb5ca0f2f71219658
Lopes Boa <lopesboa1@gmail.com>
2020-11-12 22:46:00 -0300

    looping through arrays

--- gradebook/src/Program.cs
+++ gradebook/src/Program.cs
@@ -6,8 +6,14 @@ namespace src
   {
     static void Main(string[] args)
     {
-      var numbers = new double[3];
-      numbers[0] = 12.9;
+      var numbers = new [] {3,12.2,56.3};
+      var result = 0.0;
+      
+      foreach (double number in numbers)
+      {
+          
+      }
+
       if (args.Length > 0)
       {
         Console.WriteLine($"Hello {args[0]}!");
