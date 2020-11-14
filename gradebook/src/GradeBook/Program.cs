2c2f044fed4242a6dfbf0cbda8f67baae90c4a15
Lopes Boa <lopesboa1@gmail.com>
2020-11-13 21:44:26 -0300

    adding a constructor

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -9,11 +9,10 @@ namespace GradeBook
     {
       var book = new Book();
       book.AddGrade(8.9);
-    //   var numbers = new[] { 3, 12.2, 56.3 };
+
       var grades = new List<double>() { 3, 12.2, 56.3 };
 
       grades.Add(9.8);
-    //   grades.Count;
       var result = 0.0;
 
       foreach (double number in grades)
