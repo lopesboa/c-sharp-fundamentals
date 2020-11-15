80fd89c5edfd127046e4b23f2012169f12d96eb9
Lopes Boa <lopesboa1@gmail.com>
2020-11-15 00:36:18 -0300

    working with classes and objects

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -12,6 +13,24 @@ namespace GradeBook
     public void AddGrade(double grade)
     {
       grades.Add(grade);
+    }
+
+    public void ShowStatistics()
+    {
+      var result = 0.0;
+      var highGrade = double.MinValue;
+      var lowGrade = double.MaxValue;
+
+      foreach (var number in grades)
+      {
+        lowGrade = Math.Min(number, lowGrade);
+        highGrade = Math.Max(number, highGrade);
+        result += number;
+      }
+      result /= grades.Count;
+      Console.WriteLine($"The lowest grade is {lowGrade}");
+      Console.WriteLine($"The highest grade is {highGrade}");
+      Console.WriteLine($"The avarage grade is {result:N1}");
     }
 
     private List<double> grades;
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -7,21 +7,11 @@ namespace GradeBook
   {
     static void Main(string[] args)
     {
-      var book = new Book("Lopes GradeBook");
-      book.AddGrade(8.9);
-
-      var grades = new List<double>() { 3, 12.2, 56.3 };
-
-      grades.Add(9.8);
-      var result = 0.0;
-
-      foreach (double number in grades)
-      {
-        result += number;
-      }
-      Console.WriteLine(result);
-      result /= grades.Count;
-      Console.WriteLine($"The avarage grade is {result:N2}");
+      var book = new Book("Lopes Grade Book");
+      book.AddGrade(89.3);
+      book.AddGrade(90.5);
+      book.AddGrade(77.5);
+      book.ShowStatistics();
     }
   }
 }
