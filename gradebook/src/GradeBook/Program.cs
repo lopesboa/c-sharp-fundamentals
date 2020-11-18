f7a1e5dbb9cc7b405e85ccc8d040998730df8db5
Lopes Boa <lopesboa1@gmail.com>
2020-11-17 23:10:19 -0300

    feat(branching): :sparkles: taking user input from console

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -7,11 +7,24 @@ namespace GradeBook
     static void Main(string[] args)
     {
       var book = new Book("Lopes Grade Book");
-      book.AddGrade(89.3);
-      book.AddGrade(90.5);
-      book.AddGrade(77.5);
-      var stats = book.GetStatistics();
+      // book.AddGrade(89.3);
+      // book.AddGrade(90.5);
+      // book.AddGrade(77.5);
+      var done = false;
+      while (!done)
+      {
+        Console.WriteLine("Insert a grade or press 'q' to quit");
+        var input = Console.ReadLine();
+        if(input == "q") 
+        {
+          done = true;
+          continue;
+        }
+        var grade = double.Parse(input);
+        book.AddGrade(grade);
+      }
 
+      var stats = book.GetStatistics();
       Console.WriteLine($"The lowest grade is {stats.Low}");
       Console.WriteLine($"The highest grade is {stats.High}");
       Console.WriteLine($"The avarage grade is {stats.Avarage:N1}");
